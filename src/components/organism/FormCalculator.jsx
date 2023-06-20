bb8261209ebbbe18244fcbd9fba7ac65d2727fae
LuisPOZO <luispozoengineer@gmail.com>
2023-06-20 14:59:01 -0600

    [ADD] Conjunto Universal

--- src/components/organism/FormCalculator.jsx
+++ src/components/organism/FormCalculator.jsx
@@ -5,6 +5,7 @@ function FormCalculator() {
         <>
             <WrapperInput msn="Conjunto A" type="text" placeholder="Ingresa un Conjunto" name={'conjunto_a'} />
             <WrapperInput msn="Conjunto B" type="text" placeholder="Ingresa un Conjunto" name={'conjunto_a'} />
+            <WrapperInput msn="Conjunto Universal" type="text" placeholder="Ingresa un Conjunto" name={'conjuntoUniversal'} />
         </>
      );
 }
