e65ff572ca1c9985ea7885cd93cbb84050d39955
practica <velazquezhernandezjesus13@gmail.com>
2023-07-17 20:13:58 -0600

    [ADD] view Quizz

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter,Routes, Route } from "react-router-dom";
 import Calculator from "../pages/Calculator";
+import Quizz from "../pages/Quizz";
 
 
 function App() {
     return ( 
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Calculator/>}/>
+                <Route path="Calculator" element={<Calculator/>}/>
+                <Route path="Quizz" element={<Quizz/>}/>
             </Routes>
         </BrowserRouter>
     );
