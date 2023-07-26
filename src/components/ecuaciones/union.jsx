c08c10a6905d6a2caf710238b4ffb7e716521369
practica <velazquezhernandezjesus13@gmail.com>
2023-07-25 21:35:33 -0600

    Solucion de error

--- src/components/ecuaciones/union.jsx
+++ src/components/ecuaciones/union.jsx
@@ -2,13 +2,12 @@ import ordenar from "./ordenar";
 
 function union (conjuntoA, conjuntoB) {
     const interseccionAB=new Array;
-    for (let i = 0; i<conjuntoA.length;i++){
-        for (let j = 0; j<conjuntoB.length;j++){
-            if (interseccionAB.toString().search(conjuntoB[j])==-1){
-                interseccionAB[interseccionAB.length]=conjuntoB[j];
-            }else if (interseccionAB.toString().search(conjuntoA[i])==-1){
-                interseccionAB[interseccionAB.length]=conjuntoA[i];
-            }
+    for (let j = 0; j<conjuntoB.length;j++){
+        if (interseccionAB.toString().search(conjuntoA[j])==-1){
+            interseccionAB[interseccionAB.length]=conjuntoA[j];
+        }
+        if (interseccionAB.toString().search(conjuntoB[j])==-1){
+            interseccionAB[interseccionAB.length]=conjuntoB[j];
         }
     }
     const ordenado = ordenar(interseccionAB);
